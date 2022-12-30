88189b1051d94ee422abd5c35ee5ddc28757f5cb
Saiful-Alam-Shawon <fullstackebdevelopment1@gmail.com>
2022-12-30 22:29:19 +0600

    commit

--- index.js
+++ index.js
@@ -21,54 +21,46 @@ async function run6() {
 
 
 
-        app.get('/service6/:id', async (req, res) => {
-            const id = req.params.id;
-            const query = { _id: ObjectId(id) };
-            const service = await serviceCollection6.findOne(query);
-            res.send(service)
-        })
+        // app.get('/service6/:id', async (req, res) => {
+        //     const id = req.params.id;
+        //     const query = { _id: ObjectId(id) };
+        //     const service = await serviceCollection6.findOne(query);
+        //     res.send(service)
+        // })
 
         // Added Service Page's  backend
 
         app.post('/addedTask', async (req, res) => {
             const service = req.body;
+            // console.log(service);
             const result = await allTaskCollection.insertOne(service);
+            // console.log(result);
             res.send(result)
 
         })
 
 
-        // Review Post '/service6/:id'
-
-        app.post('/reviews', async (req, res) => {
-            const reviews = req.body;
-            const review = await reviewCollection.insertOne(reviews);
-            console.log(review);
-            res.send(review)
-        })
 
 
-        // (xomovid679@fgvod.com,nosel60130@klblogs.com,wenekex651@jernang.com)
 
+        // Task Get by email
 
-        // Review Get by email
-
-        app.get('/reviews', async (req, res) => {
+        app.get('/getTaskByMail', async (req, res) => {
             let query = {}
             if (req?.query?.email) {
                 query = {
                     email: req.query.email
                 }
             }
-            const cursor = reviewCollection.find(query);
+            const cursor = allTaskCollection.find(query);
             const review = await cursor.toArray();
             res.send(review);
         })
 
 
-        // Show Specific Review on base id
+        // Show Specific Task on base id
 
-        app.get('/review', async (req, res) => {
+        app.get('/task', async (req, res) => {
             let query = {}
             if (req?.query?.id) {
                 query = {
@@ -80,10 +72,28 @@ async function run6() {
             res.send(review);
         });
 
+        // Task Put test
+
+        app.put('/edit/:id', async (req, res) => {
+            // console.log(req);
+            const id = req.params.id;
+            // console.log(id);
+            const filter = { _id: ObjectId(id) };
+            const options = { upsert: true };
+            const updatedDoc = {
+                $set: req.body
+            };
+            // console.log(updatedDoc);
+            const result = await allTaskCollection.updateOne(filter, updatedDoc, options);
+            // res.send(result);
+        })
+
+
+        // Task Deleted
 
         app.delete('/task/:id', async (req, res) => {
+            // console.log(req)
             const id = req.params.id;
-            // console.log(id)
             const query = { _id: ObjectId(id) };
             const result = await allTaskCollection.deleteOne(query);
             res.send(result);
